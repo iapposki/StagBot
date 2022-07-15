e78b67f49a982ee7a4a2647210c179ae1442975b
iapposki <konark981999@gmail.com>
2022-07-15 19:16:54 +0530

    added welcome greeting direct message functionality

---
+++ helperFunctions.py
@@ -0,0 +1,6 @@
+from msilib.schema import Class
+import discord
+
+class Stag:
+    def __init__(self) -> None:
+        pass
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-import json
 import discord
 import os
 from dotenv import load_dotenv
+# from helperFunctions import *
 
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
-bot = discord.Client()
+intents = discord.Intents().all()
+
+bot = discord.Client(intents=intents)
+
 
 # def guard_zero(func):
 #     def inner(x,y):
@@ -30,16 +33,32 @@ async def on_ready():
     guild_count = 0
 
     for guild in bot.guilds:
-        print(f"- {guild.id} {guild.name}")
+        print(f"(@) {guild.id} {guild.name}")
+        members = '\n - '.join([member.name for member in guild.members])
+        print(f"Guild Members in the Guild {guild.name} :\n - {members}")
         guild_count += 1
 
     print(f"StagBot is in {str(guild_count)} server(s).")
 
+@bot.event
+async def on_member_join(member):
+    await member.create_dm()
 
 @bot.event
 async def on_message(message):
-    if message.content == "hello":
-        await message.channel.send("hey there!")
+    try:
+        if message.content[0] == "~":
+            commands = message.content.split(" ")
+            # print(commands)
+            # if commands[0] == "~get":
+            await message.channel.send("command recognized")
+    except:
+        pass
+
+@bot.event 
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(f'Hi {member.name}!\nWelcome to BuzzFeed Gaming!')
 
 
 bot.run(DISCORD_TOKEN)
