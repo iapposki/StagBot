7226f1ecb62f4527ad3dd0c3ed8990ddc3fa626b
iapposki <konark981999@gmail.com>
2022-08-02 16:16:03 +0530

    minor changes

--- utilities.py
+++ utilities.py
@@ -6,13 +6,17 @@ async def get_free_games(context):
     from libraries import get_free_epic_games, get_free_steam_games
     data = get_free_epic_games()
     data += get_free_steam_games()
-    for i in range(len(data)):
-        try:
-            url = data[i].store_link
-            img_url = data[i].image_url
-            embedVar = discord.Embed(title=data[i].title, url=url, color=0x00ff00)
-            embedVar.set_image(url=img_url)
-            await context.channel.send(embed=embedVar)
-        except Exception as err:
-            print("Error : ", sys.exc_info()[0], "occurred.")
-            print(err)
+    data = []
+    if len(data) == 0:
+        await context.channel.send("No free games for now!")
+    else:
+        for i in range(len(data)):
+            try:
+                url = data[i].store_link
+                img_url = data[i].image_url
+                embedVar = discord.Embed(title=data[i].title, url=url, color=0x00ff00)
+                embedVar.set_image(url=img_url)
+                await context.channel.send(embed=embedVar)
+            except Exception as err:
+                print("Error : ", sys.exc_info()[0], "occurred.")
+                print(err)
